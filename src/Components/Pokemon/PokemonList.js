af36b4cdd30f214949676aec28e7c4e3a5c5b073
Jeisson Gomez <jeypitbullz4@gmai.com>
2020-09-16 12:17:01 -0500

    Correccion del componente pokemonList

--- src/Components/Pokemon/PokemonList.js
+++ src/Components/Pokemon/PokemonList.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react'
 import PokemonCard from './PokemonCard'
 
 export default class PokemonList extends Component{
-    
+    state = {
+        url:'https://pokeapi.co/api/v2/pokemon/',
+        pokemon: null
+    };
+
     render() {
         return (
             <div className = "row">
