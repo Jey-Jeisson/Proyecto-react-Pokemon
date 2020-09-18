9c288406ff4facd8891b98c602977faac7055a99
Jeisson Gomez <jeypitbullz4@gmai.com>
2020-09-18 11:19:39 -0500

    modificacion del proyecto

--- src/Components/Layout/NavBar.js
+++ src/Components/Layout/NavBar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 
@@ -7,11 +7,9 @@ export default class NavBar extends Component {
         return (
             <div>
                 <nav className = "navbar navbar-expand-nd navbar-dark bg-dark fixed-top">
-                    <a className = "navbar-brand col-sm-3 col-sm-2 mr-0 align-itms-center">
-                        pokemon
-                    </a>
+                  <a>pokemon</a>
                 </nav>
             </div>
-        )
+        );
     }
 }
--- src/Components/Pokemon/PokemonList.js
+++ src/Components/Pokemon/PokemonList.js
@@ -7,21 +7,25 @@ export default class PokemonList extends Component{
         url:'https://pokeapi.co/api/v2/pokemon/',
         pokemon: null
     };
+    
+    async componentDidMount(){
+      const res = await axios.get(this.state.url);
+      this.setState({pokemon: res.data['results']});
+    }
 
     render() {
         return (
-            <div>
-            {this.state.pokemon ? (
-            <div className = "row">
-            {this.state.pokemon.map(pokemon => (
-                <PokemonCard />
-            ))}
-        </div>) : (
-        <h1>
-            Loading pokemon
-        </h1>
-        )}
-        </div>
+            <React.Fragment>
+                {this.state.pokemon ? (
+                  <div className = "row">
+                      {this.state.pokemon.map(pokemon => (
+                        <PokemonCard />
+                      ))}
+                    </div>
+                ) : (
+                  <h1>Loading pokemon</h1>
+                )}
+            </React.Fragment>
         )
     }
 }
